62d4054192006f1a41317775848b894010c86117
Max <btcthirst@gmail.com>
2021-02-26 16:32:24 +0200

    third exercise

--- main.go
+++ main.go
@@ -4,15 +4,26 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
-func main() {
+func getPots(n string) {
 
-	conn, err := http.Get("https://jsonplaceholder.typicode.com/posts/")
+	conn, err := http.Get(n)
 	if err != nil {
 		panic(err)
 	}
 
 	body, err := ioutil.ReadAll(conn.Body)
 	fmt.Println(string(body))
+}
+
+func main() {
+	for i := 1; i < 100; i++ {
+		text := fmt.Sprintf("https://jsonplaceholder.typicode.com/posts/%d/", i)
+		go getPots(text)
+	}
+
+	time.Sleep(time.Second)
+
 }
